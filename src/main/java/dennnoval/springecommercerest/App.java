227da078394582cb324d5773b1193703dd04250d
dennnoval <dennnoval@gmail.com>
2022-06-25 21:53:04 +0700

    Create App.java

---
+++ src/main/java/dennnoval/springecommercerest/App.java
@@ -0,0 +1,13 @@
+package dennnoval.springecommercerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+}
