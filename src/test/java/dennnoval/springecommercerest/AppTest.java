311654cbe1501311af210b6f8b4ee34fda022b93
dennnoval <dennnoval@gmail.com>
2022-06-25 21:53:09 +0700

    Create AppTest.java

---
+++ src/test/java/dennnoval/springecommercerest/AppTest.java
@@ -0,0 +1,15 @@
+package dennnoval.springecommercerest;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+  @Test
+  void exampeTest() {
+    // Write here
+  }
+
+}
