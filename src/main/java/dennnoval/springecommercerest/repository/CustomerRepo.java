cc43a3384c6df173d707c54fe30b750806bf0484
dennnoval <dennnoval@gmail.com>
2022-06-26 22:19:00 +0700

    Create CustomerRepo.java

---
+++ src/main/java/dennnoval/springecommercerest/repository/CustomerRepo.java
@@ -0,0 +1,9 @@
+package dennnoval.springecommercerest.repository;
+
+import dennnoval.springecommercerest.entity.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepo extends PagingAndSortingRepository<Customer, Integer> {
+}
