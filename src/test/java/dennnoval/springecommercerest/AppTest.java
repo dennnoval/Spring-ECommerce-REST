f7cb6502ae0e6cf247bc97c80977a4d625bf8fa4
dennnoval <dennnoval@gmail.com>
2022-06-27 14:38:56 +0700

    Update AppTest.java

--- src/test/java/dennnoval/springecommercerest/AppTest.java
+++ src/test/java/dennnoval/springecommercerest/AppTest.java
@@ -3,11 +3,12 @@ package dennnoval.springecommercerest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 public class AppTest {
 
-  @Test
-  void exampeTest() {
+  // method naming format : UnitOfWork_StateUnderTest_ExpectedBehaviour
+  // @Test
+  void unitOfWork_stateUnderTest_expectedBehaviour() {
     // Write here
   }
 
