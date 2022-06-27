a3a2e3005cc09bf602e95a7c3204e0d78fc7cd54
dennnoval <dennnoval@gmail.com>
2022-06-27 14:38:49 +0700

    Update OrderRepo.java

--- src/main/java/dennnoval/springecommercerest/repository/OrderRepo.java
+++ src/main/java/dennnoval/springecommercerest/repository/OrderRepo.java
@@ -4,6 +4,11 @@ import dennnoval.springecommercerest.entity.Order;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Spliterator;
+
 @RepositoryRestResource
 public interface OrderRepo extends PagingAndSortingRepository<Order, Integer> {
+
+  Spliterator<Order> findAllByCustomerUsername(String username);
+
 }
